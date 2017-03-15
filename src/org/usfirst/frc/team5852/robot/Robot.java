1be11e9fe3131c622ddec5897d35a0d7600c81f3
Jacob Frederick <jakeafred1012@gmail.com>
2017-03-15 10:46:28 -0700

    Changed Autonomous Coding and Cleaned Up Code

--- src/org/usfirst/frc/team5852/robot/Robot.java
+++ src/org/usfirst/frc/team5852/robot/Robot.java
@@ -6,18 +6,20 @@ import edu.wpi.first.wpilibj.networktables.NetworkTable;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj.AnalogInput;
+import org.opencv.core.Mat;
+import org.opencv.imgproc.Imgproc;
+import edu.wpi.cscore.CvSink;
+import edu.wpi.cscore.CvSource;
+import edu.wpi.cscore.MjpegServer;
+import edu.wpi.cscore.UsbCamera;
+import edu.wpi.cscore.VideoMode.PixelFormat;
+import edu.wpi.first.wpilibj.CameraServer;
 // class -> (not using)robotinit -> autoinit -> autoPeriodic -> teleopinit -> telopPeriodic
 
 public class Robot extends IterativeRobot {
 	//Command autocommand;
 	@SuppressWarnings("rawtypes")
 	private SendableChooser chooser;
-
-	//final String defaultAuto = "Default";
-
-	//final String tripleR = "tripleR";
-
-	String autoSelected;
 	
 	//Analog Input for Ultrasonic Sensor
 	AnalogInput ultra = new AnalogInput(0);
@@ -51,8 +53,7 @@ public class Robot extends IterativeRobot {
 	int Xaxis = 0;
 	int buttonA = 2;
 	int buttonY = 4;
-	int ForTrigger = 0;
-	int ReverseBut = 1;
+	int ForTrigger = 1;
 	int centerx = 320;
 	int centery = 240;
 	
@@ -63,11 +64,21 @@ public class Robot extends IterativeRobot {
 	@SuppressWarnings({ "rawtypes", "unchecked" })
 	public void robotInit() 
 	{
+		UsbCamera usbcam = new UsbCamera("USB0", 0);
+		MjpegServer mserve = new MjpegServer("serv", 1181);
+		mserve.setSource(usbcam);
+		CvSink sink = new CvSink("cvcam0");
+		sink.setSource(usbcam);
+		
+		CvSource output = new CvSource("output", PixelFormat.kMJPEG,320,240,30);
+		
+		MjpegServer mjpg = new MjpegServer("mjp", 1182);
+		mjpg.setSource(output);
 
 		chooser = new SendableChooser();
 		chooser.addDefault("No Auto", 1);
-		chooser.addObject("Forward & Gear", 2);
-		chooser.addObject("Gear & Cam", 3);
+		chooser.addObject("Baseline", 2);
+		chooser.addObject("Gear", 3);
 		SmartDashboard.putData("Auto choices", chooser);
 		
 		NetworkTable.setIPAddress("10.58.52.2");
@@ -79,9 +90,10 @@ public class Robot extends IterativeRobot {
 	{
 
 		mode = (int) chooser.getSelected();
-		
+		System.out.println("Auto selected: " + mode);
 	}
 
+	@SuppressWarnings("deprecation")
 	public void autonomousPeriodic() 
 	{
 		Scheduler.getInstance().run();
@@ -98,19 +110,13 @@ public class Robot extends IterativeRobot {
 		}else if (mode == 2)
 		{
 			
-			//Forward & Gear Autonomous
+			//Baseline Autonomous
 			while (isAutonomous() && isEnabled())
 			{
-				//Approaches Obstacle at Half Speed
-				for (int i = 0; i < 80000; i++) 
+				//Approaches Baseline at Half Speed
+				for (int i = 0; i < 200000; i++) 
 				{
-					drive.tankDrive(0.5, 0.5);
-				}
-				//Guns It Over The Obstacle
-				Timer.delay(2);
-				for (int i = 0; i < 200000; i++)
-				{
-					drive.tankDrive(1, 1);
+					drive.tankDrive(0.65, 0.65);
 				}
 				//Delays the Autonomous for the Rest of the Autonomous Period and Breaks the Loop
 				Timer.delay(13);
@@ -119,10 +125,41 @@ public class Robot extends IterativeRobot {
 			}
 		}else if (mode == 3)
 		{
-			//Gear & Cam Autonomous
-			while (isAutonomous() && isEnabled())
-			{
-
+			//Gear Autonomous
+			
+			while (isAutonomous() && isEnabled()){
+				/*i would take care of getting to the proper distance first. depending on where you mount the camera
+		it will determine the proper "height" you need to be at. I would get to a safe clearance distance that allows
+		room to rotate for allignment*/
+				int Y=0, X=0;
+				table.getInt("Y",Y);
+				//example, you will need to get the proper values on friday.
+				if (Y != centery)
+				{
+					drive.tankDrive(0.5, 0.5);
+					table.getInt("Y", Y);
+				}
+				drive.tankDrive(0, 0);
+				table.getInt("X",X);
+				if (X != centerx)
+				{
+				//not sure if this is correct, you will need to figure out what +/- values rotate robot to the right and left
+					if (X < 300)
+					{
+						drive.tankDrive(0.5, -0.5);	
+					}
+					else if(X > 360)
+					{
+						drive.tankDrive(-0.5, 0.5);
+					}
+					table.getInt("X",X);
+				}
+				//now inch forward a little to get on the peg, again, you need to test these values.
+				for (int i=0; i<1000; i++)
+				{
+					drive.tankDrive(0.3, 0.3);
+				}
+				break;
 			}
 		}
 		
@@ -160,10 +197,6 @@ public class Robot extends IterativeRobot {
 			if(flightstick.getRawButton(ForTrigger))
 			{
 				climber.set(1);
-			}
-			else if(flightstick.getRawButton(ReverseBut))
-			{
-				climber.set(-1);
 			}
 			else
 			{
